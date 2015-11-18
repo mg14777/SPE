fbbf5682ce252bfba9d3c2e9a690d05bacf26cf4
Administrator <kiran@kiran-AOA110.(none)>
2015-11-18 15:14:44 +0000

    cors started

---
+++ front-end/js/communication.js
@@ -0,0 +1,33 @@
+var url = "locationOfServer";
+
+function ajaxObject(){
+    var xhttp;
+    if (window.XMLHttpRequest) {
+        xhttp = new XMLHttpRequest();
+    } else {
+        // code for IE6, IE5
+        xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    return xhttp;
+}
+
+function sendRequest(dataObject, type){
+    var json = JSON.stringify(dataObject);
+    var xhttp = ajaxObject();
+    var returnObject;
+
+    xhttp.onreadystatechange = function() {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            var text = xhttp.responseText;
+            var obj = JSON.parse(text);
+            updateScreen(object);
+        }
+    };
+
+    xhttp.open("GET", url, true);
+    xhttp.send();    
+}
+
+function updateScreen(object){
+
+}
